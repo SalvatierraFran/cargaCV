bdec36cf24dff308a8743cca50d25dbc857b960f
SOFTTEK\franco.salvatierra <asdasd>
2018-03-21 16:20:04 -0300

    Se le agregó modificación a los datos personales

--- app/src/main/java/com/example/francosalvatierra/cargacv/DatosFragment.java
+++ app/src/main/java/com/example/francosalvatierra/cargacv/DatosFragment.java
@@ -28,6 +28,7 @@ import java.util.ArrayList;
 public class DatosFragment extends Fragment {
 
     DatosPersonales auxDatos;
+    String queHacer = "guardar";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,9 +41,12 @@ public class DatosFragment extends Fragment {
 
         final Integer idCred = ((Integer)bundle.get("Datos")).intValue();
 
+        ((Button)v.findViewById(R.id.modificar_btndatos)).setVisibility(View.INVISIBLE);
+
         if(comprobar(idCred, v))
         {
             ((Button)v.findViewById(R.id.guardar_btndatos)).setVisibility(View.INVISIBLE);
+            ((Button)v.findViewById(R.id.modificar_btndatos)).setVisibility(View.VISIBLE);
         }
 
         ((Button)v.findViewById(R.id.guardar_btndatos)).setOnClickListener(new View.OnClickListener() {
@@ -64,6 +68,8 @@ public class DatosFragment extends Fragment {
 
     public void modificar(Integer idCred, View v)
     {
+        queHacer = "modificar";
+
         EditText nombre_et = (EditText)getView().findViewById(R.id.nombre_etdatos);
         nombre_et.setEnabled(true);
 
@@ -84,6 +90,9 @@ public class DatosFragment extends Fragment {
 
         EditText ecivil_et = (EditText)getView().findViewById(R.id.ecivil_etdatos);
         ecivil_et.setEnabled(true);
+
+        ((Button)getView().findViewById(R.id.guardar_btndatos)).setVisibility(View.VISIBLE);
+        ((Button)getView().findViewById(R.id.modificar_btndatos)).setVisibility(View.INVISIBLE);
     }
 
     public Boolean comprobar(Integer idCred, View v)
@@ -106,59 +115,64 @@ public class DatosFragment extends Fragment {
 
         if(result) {
 
-            EditText nombre_et = (EditText) v.findViewById(R.id.nombre_etdatos);
-
-            EditText apellido_et = (EditText) v.findViewById(R.id.apellido_etdatos);
-
-            EditText edad_et = (EditText) v.findViewById(R.id.edad_etdatos);
-
-            EditText nacio_et = (EditText) v.findViewById(R.id.nacio_etdatos);
-
-            EditText pcia_et = (EditText) v.findViewById(R.id.pcia_etdatos);
-
-            EditText domi_et = (EditText) v.findViewById(R.id.domi_etdatos);
-
-            EditText ecivil_et = (EditText) v.findViewById(R.id.ecivil_etdatos);
-
-            nombre_et.setText(auxDatos.getNombre().toString());
-            nombre_et.setEnabled(false);
-
-            apellido_et.setText(auxDatos.getApellido().toString());
-            apellido_et.setEnabled(false);
-
-            edad_et.setText(auxDatos.getEdad().toString());
-            edad_et.setEnabled(false);
-
-            nacio_et.setText(auxDatos.getNacionalidad().toString());
-            nacio_et.setEnabled(false);
-
-            pcia_et.setText(auxDatos.getProvincia().toString());
-            pcia_et.setEnabled(false);
-
-            domi_et.setText(auxDatos.getDomicilio().toString());
-            domi_et.setEnabled(false);
-
-            ecivil_et.setText(auxDatos.getEcivil().toString());
-            ecivil_et.setEnabled(false);
+            bloquear(v);
         }
         return result;
+    }
+
+    public void bloquear(View v)
+    {
+        EditText nombre_et = (EditText) v.findViewById(R.id.nombre_etdatos);
+
+        EditText apellido_et = (EditText) v.findViewById(R.id.apellido_etdatos);
+
+        EditText edad_et = (EditText) v.findViewById(R.id.edad_etdatos);
+
+        EditText nacio_et = (EditText) v.findViewById(R.id.nacio_etdatos);
+
+        EditText pcia_et = (EditText) v.findViewById(R.id.pcia_etdatos);
+
+        EditText domi_et = (EditText) v.findViewById(R.id.domi_etdatos);
+
+        EditText ecivil_et = (EditText) v.findViewById(R.id.ecivil_etdatos);
+
+        nombre_et.setText(auxDatos.getNombre().toString());
+        nombre_et.setEnabled(false);
+
+        apellido_et.setText(auxDatos.getApellido().toString());
+        apellido_et.setEnabled(false);
+
+        edad_et.setText(auxDatos.getEdad().toString());
+        edad_et.setEnabled(false);
+
+        nacio_et.setText(auxDatos.getNacionalidad().toString());
+        nacio_et.setEnabled(false);
+
+        pcia_et.setText(auxDatos.getProvincia().toString());
+        pcia_et.setEnabled(false);
+
+        domi_et.setText(auxDatos.getDomicilio().toString());
+        domi_et.setEnabled(false);
+
+        ecivil_et.setText(auxDatos.getEcivil().toString());
+        ecivil_et.setEnabled(false);
     }
 
     public void guardar(Integer idCred)
     {
         EditText nombre_et = (EditText) getView().findViewById(R.id.nombre_etdatos);
 
-        EditText apellido_et = (EditText)getView().findViewById(R.id.apellido_etdatos);
+        EditText apellido_et = (EditText) getView().findViewById(R.id.apellido_etdatos);
 
-        EditText edad_et = (EditText)getView().findViewById(R.id.edad_etdatos);
+        EditText edad_et = (EditText) getView().findViewById(R.id.edad_etdatos);
 
-        EditText nacio_et = (EditText)getView().findViewById(R.id.nacio_etdatos);
+        EditText nacio_et = (EditText) getView().findViewById(R.id.nacio_etdatos);
 
-        EditText pcia_et = (EditText)getView().findViewById(R.id.pcia_etdatos);
+        EditText pcia_et = (EditText) getView().findViewById(R.id.pcia_etdatos);
 
-        EditText domi_et = (EditText)getView().findViewById(R.id.domi_etdatos);
+        EditText domi_et = (EditText) getView().findViewById(R.id.domi_etdatos);
 
-        EditText ecivil_et = (EditText)getView().findViewById(R.id.ecivil_etdatos);
+        EditText ecivil_et = (EditText) getView().findViewById(R.id.ecivil_etdatos);
 
         CVitaesDbHelper conn = new CVitaesDbHelper(this.getContext(), "CVitaesDB", null, CVitaesDbHelper.DATABASE_VERSION);
 
@@ -166,17 +180,41 @@ public class DatosFragment extends Fragment {
 
         ContentValues values = new ContentValues();
 
-        values.put(DatosContract.DatosEntry.CAMPO_NOMBRE, nombre_et.getText().toString());
-        values.put(DatosContract.DatosEntry.CAMPO_APELLIDO, apellido_et.getText().toString());
-        values.put(DatosContract.DatosEntry.CAMPO_EDAD, edad_et.getText().toString());
-        values.put(DatosContract.DatosEntry.CAMPO_NACION, nacio_et.getText().toString());
-        values.put(DatosContract.DatosEntry.CAMPO_PCIA, pcia_et.getText().toString());
-        values.put(DatosContract.DatosEntry.CAMPO_DOMI, domi_et.getText().toString());
-        values.put(DatosContract.DatosEntry.CAMPO_ECIVIL, ecivil_et.getText().toString());
-        values.put(DatosContract.DatosEntry.CAMPO_IDCRED, idCred);
+        if(queHacer.equals("guardar"))
+        {
+            values.put(DatosContract.DatosEntry.CAMPO_NOMBRE, nombre_et.getText().toString());
+            values.put(DatosContract.DatosEntry.CAMPO_APELLIDO, apellido_et.getText().toString());
+            values.put(DatosContract.DatosEntry.CAMPO_EDAD, edad_et.getText().toString());
+            values.put(DatosContract.DatosEntry.CAMPO_NACION, nacio_et.getText().toString());
+            values.put(DatosContract.DatosEntry.CAMPO_PCIA, pcia_et.getText().toString());
+            values.put(DatosContract.DatosEntry.CAMPO_DOMI, domi_et.getText().toString());
+            values.put(DatosContract.DatosEntry.CAMPO_ECIVIL, ecivil_et.getText().toString());
+            values.put(DatosContract.DatosEntry.CAMPO_IDCRED, idCred);
 
-        Long idResultante = db.insert(DatosContract.DatosEntry.TABLE_NAME, DatosContract.DatosEntry._ID, values);
+            Long idResultante = db.insert(DatosContract.DatosEntry.TABLE_NAME, DatosContract.DatosEntry._ID, values);
 
-        Toast.makeText(this.getActivity().getApplicationContext(), "Id Registro: " + idResultante, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this.getActivity().getApplicationContext(), "Id Registro: " + idResultante, Toast.LENGTH_SHORT).show();
+
+            ((Button)getView().findViewById(R.id.guardar_btndatos)).setVisibility(View.INVISIBLE);
+            ((Button)getView().findViewById(R.id.modificar_btndatos)).setVisibility(View.VISIBLE);
+        }
+        if(queHacer.equals("modificar"))
+        {
+            values.put(DatosContract.DatosEntry.CAMPO_NOMBRE, nombre_et.getText().toString());
+            values.put(DatosContract.DatosEntry.CAMPO_APELLIDO, apellido_et.getText().toString());
+            values.put(DatosContract.DatosEntry.CAMPO_EDAD, edad_et.getText().toString());
+            values.put(DatosContract.DatosEntry.CAMPO_NACION, nacio_et.getText().toString());
+            values.put(DatosContract.DatosEntry.CAMPO_PCIA, pcia_et.getText().toString());
+            values.put(DatosContract.DatosEntry.CAMPO_DOMI, domi_et.getText().toString());
+            values.put(DatosContract.DatosEntry.CAMPO_ECIVIL, ecivil_et.getText().toString());
+            values.put(DatosContract.DatosEntry.CAMPO_IDCRED, idCred);
+
+            db.update("Datos", values, "IdCred = " + idCred, null);
+
+            Toast.makeText(this.getActivity().getApplicationContext(), "Actualizado correctamente.", Toast.LENGTH_SHORT).show();
+
+            ((Button)getView().findViewById(R.id.guardar_btndatos)).setVisibility(View.INVISIBLE);
+            ((Button)getView().findViewById(R.id.modificar_btndatos)).setVisibility(View.VISIBLE);
+        }
     }
 }
